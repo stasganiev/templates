999769b154c8eb61fc7821a83c2c0b574cbd01ff
Stas Ganiev <stas@ganiev.pro>
2020-12-13 13:11:59 +1000

    Расширение архитектуры со сборкой и pug

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
 
 var gulp = require("gulp");
 var sass = require("gulp-sass");
-
+var pug = require('gulp-pug');
 var plumber = require("gulp-plumber");
 var sourcemap = require("gulp-sourcemaps");
 var rename = require("gulp-rename");
@@ -11,54 +11,59 @@ var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
 var csso = require("gulp-csso");
 
-gulp.task("css", function () {
-  return gulp.src("src/sass/style.scss")
+gulp.task("fonts", ()=>{
+  return gulp.src("src/fonts/**/*.*")
+    .pipe(gulp.dest("public/fonts"));
+});
+
+gulp.task("img", ()=>{
+  return gulp.src(["src/img/**/*.*", "src/elements/**/*.svg", "src/elements/**/*.jpg", "src/elements/**/*.png"])
+    .pipe(gulp.dest("public/img"));
+});
+
+gulp.task("js", ()=>{
+  return gulp.src("src/js/**/*.js")
+    .pipe(gulp.dest("public/js"));
+});
+
+gulp.task("pug", ()=>{
+  return gulp.src(["src/pug/**/*.pug", "!src/pug/templates/**/*.pug"])
+    .pipe(pug({pretty:true}))
+    .pipe(gulp.dest('public'))
+});
+
+gulp.task("css", ()=>{
+  return gulp.src("src/sass/index.scss")
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(sass())
     .pipe(postcss([
       autoprefixer()
     ]))
-    // .pipe(csso())
+    .pipe(csso())
     .pipe(rename("style_min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("src/css"))
+    .pipe(gulp.dest("public/css"))
     .pipe(server.stream());
 });
 
-gulp.task("cssnorm", function () {
-  return gulp.src("src/sass/normalize.scss")
-    .pipe(plumber())
-    .pipe(sourcemap.init())
-    .pipe(sass())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
-    // .pipe(csso())
-    .pipe(rename("normalize_min.css"))
-    .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("src/css"))
-    .pipe(server.stream());
-});
-
-gulp.task("server", function () {
+gulp.task("server", ()=>{
   server.init({
-    server: "src/",
+    server: "public/",
     notify: false,
     open: true,
     cors: true,
     ui: false
   });
 
+  gulp.watch("src/elements/**/*.*", gulp.series("img", "js", "pug", "css"));
+  gulp.watch("src/fonts/**/*.*", gulp.series("fonts"));
+  gulp.watch("src/img/**/*.*", gulp.series("img"));
+  gulp.watch("src/js/**/*.js", gulp.series("js"));
+  gulp.watch("src/pug/**/*.pug", gulp.series("pug"));
   gulp.watch("src/sass/**/*.scss", gulp.series("css"));
   gulp.watch("src/*.html").on("change", server.reload);
 });
 
-gulp.task("build", gulp.series("css", "cssnorm"));
+gulp.task("build", gulp.series("fonts", "img", "js", "pug", "css"));
 gulp.task("start", gulp.series("build", "server"));
-
-// gulp.task("task_name", function () {
-//     return gulp.src("source_files") // путь к файлам-исходникам
-//     .pipe(plugin_name()) // название плагина, через который прогоняем их
-//     .pipe(gulp.dest("destination")) // путь к папке, куда помещаем конечные файлы
-// })
